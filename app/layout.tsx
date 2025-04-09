26bc585cbdaa0c8922af230ef573a3e6c522a589
TheeHk-blip <muguropeter15@gmail.com>
2025-04-09 13:21:23 +0300

    Added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import "./globals.css";
 import { Navbar } from "./components/navbar";
 import { siteConfig } from "./config/site";
@@ -35,6 +37,7 @@ export default function RootLayout({
           <Navbar/>     
           <main className="container pt-5 pl-1.5 z-auto" >
             {children} 
+            <SpeedInsights />
           </main>     
         </div>        
       </Providers>     
