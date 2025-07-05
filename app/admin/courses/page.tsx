4a3241024d6c78d8382f6e3f4adc52b7f1e56736
TheeHk-blip <muguropeter15@gmail.com>
2025-07-05 07:35:20 +0300

    fixes

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,21 +1,8 @@
-/*import { title } from "@/app/components/primitives";
-import Course from "@/app/models/Course";
-import dbConnect from "@/db/courseConnect";
-import { Create, EditAttributesTwoTone } from "@mui/icons-material";
-import { Tooltip } from "@mui/material";
-import Link from "next/link";
-import { NextResponse } from "next/server";
+import { title } from "@/app/components/primitives";
+
 
 
 export default async function Courses() {
-  let courses = []
-  try {
-    await dbConnect();
-    courses =  await Course.find({});
-  } catch (error) {
-    console.error("No courses found:", error)
-    return NextResponse.json("No courses found")
-  }
 
   return (
     <div className="flex flex-col justify-center flex-shrink sm:flex-wrap">
@@ -36,43 +23,11 @@ export default async function Courses() {
               <th className="Table" >Action</th>
             </tr>
           </thead>
-          <tbody className="cursor-default" >
-            {courses.length > 0 ? (
-              courses.map((course) => (
-                <tr key={course._id}>                                    
-                    <td className="Table" >{course.courseCode}</td>                 
-                  <Tooltip title={course.courseTitle} placement={"top"} >
-                    <td className="Table" >{course.courseTitle}</td>
-                  </Tooltip>                                                   
-                    <td className="Table" >{course.courseDuration}</td>                 
-                    <td className="Table" >{course.courseFee}</td>                 
-                  <Tooltip title={course.courseDetails} placement={"right"} >
-                    <td className="Table" >{course.courseDetails}</td>  
-                  </Tooltip>                
-                  <Tooltip title={course.courseUnits} placement={"left"} >
-                    <td className="Table" >{course.courseUnits}</td>
-                  </Tooltip>                                   
-                  <td className="Table" >
-                    <Link href={"/admin/courses/editCourse"} className="text-blue-500 hover:underline" >
-                      <EditAttributesTwoTone/>
-                      Edit
-                    </Link>
-                    <Link href={"/admin/courses/createCourse"} className="text-blue-500 hover:underline">
-                      <Create/>
-                      Create
-                    </Link>
-                  </td>
-                </tr>
-              ))
-            ):(
-              <div>
-                <span>No courses found</span>
-              </div>
-            )}                          
+          <tbody className="cursor-default" >                        
           </tbody>
         </table>
         </div>
       </div>
     </div>
   )  
-}*/
+}
